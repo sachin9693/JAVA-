a1c43610ccf87b949452f01f8dfa0c672cf7c7c4
sachin <sachinsingh969321@gmail.com>
2025-05-18 09:55:40 +0530

    ADD TWO NUMBERS

---
+++ add_Two_Number.java
@@ -0,0 +1,10 @@
+public class add_Two_Number {
+    public static void main(String[] args) {
+        int num_1 = 10;
+        int num_2 = 20;
+
+        int total = num_1 + num_2;
+
+        System.out.println("Total number : "+ total);
+    }
+}
