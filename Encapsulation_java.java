41e7f687a534d16fa8cacedd3a3a265e6ac6139f
sachin <sachinsingh969321@gmail.com>
2025-05-19 18:07:05 +0530

    java Encapsulation Program

--- Encapsulation_java.java
+++ Encapsulation_java.java
@@ -1,4 +1,60 @@
 
+// class Accoun{
+//     private  long acco_no;
+//     private String name , email_id ;
+//     private double amount;
+
+//     public long getAcco_no() {
+//         return acco_no;
+//     }
+
+//     public void setAcco_no(long acco_no) {
+//         this.acco_no = acco_no;
+//     }
+
+//     public String getEmail_id() {
+//         return email_id;
+//     }
+// //    public String setEmail_id(String email_id){
+// //
+// //         this.email_id = email_id;
+// //    }
+
+
+//     public void setEmail_id(String email_id) {
+//         this.email_id = email_id;
+//     }
+
+//     public String getName() {
+//         return name;
+//     }
+
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+
+//     public double getAmount() {
+//         return amount;
+//     }
+
+//     public void setAmount(double amount) {
+//         this.amount = amount;
+//     }
+// }
+
+// public class Encapsulation_java {
+
+//     public static void main(String[] args) {
+//         Accoun ac = new Accoun();
+
+//         ac.setAcco_no(123456789);
+//         ac.setName("Sachin Singh");
+//         ac.setEmail_id("sachin9693@gmail.com");
+//         ac.setAmount(100000);
+
+//     }
+// }
+
 class Accoun{
     private  long acco_no;
     private String name , email_id ;
@@ -40,7 +96,15 @@ class Accoun{
     public void setAmount(double amount) {
         this.amount = amount;
     }
+
+    public  void display(){
+        System.out.println("name is : "+ name);
+        System.out.println("Email id is : "+email_id);
+        System.out.println("Account number : "+acco_no);
+        System.out.println("Amount : "+amount);
+    }
 }
+
 
 public class Encapsulation_java {
 
@@ -51,6 +115,7 @@ public class Encapsulation_java {
         ac.setName("Sachin Singh");
         ac.setEmail_id("sachin9693@gmail.com");
         ac.setAmount(100000);
+        ac.display();
 
     }
 }
